b7f41e07c0a904c89b36fbaba0610cd0209a424c
Arthur <ayatsun@ukr.net>
2018-04-10 15:12:17 +0300

    First commit

---
+++ src/Controller.java
@@ -0,0 +1,8 @@
+public class Controller {
+
+    public String demichialToBinary(){
+
+        return "1";
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Arthur on 10.04.2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
