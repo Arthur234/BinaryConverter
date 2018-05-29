b3384ac5ab3b3c60a920e22e7fcb38d0e0099f2b
Arthur234 <ayatsun@ukr.net>
2018-05-29 23:37:50 +0300

    Improve to 16-system

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,10 @@
-import binary.BinToDec;
-import decimal.DecToBin;
-import decimal.DecToHex;
 import decimal.Dechimal;
 
 public class Main{
 
     public static void main(String[] args) {
 
-        DecToBin dtb = new DecToBin();
-        DecToHex dth = new DecToHex();
-        BinToDec btd = new BinToDec();
-
-//        int[] ar = {1, 0, 1, 1};
-//        System.out.println(btd.binToDec(ar));
-
-//        System.out.println(dtb.posDemToBinaryString(14));
-//        System.out.println(dtb.negDemToBinaryString(-14));
-
-//        System.out.println(dth.decToHex((double)320/16));
-
         Dechimal d = new Dechimal();
-
-        d.calculate(400, 16);
+        d.calculate(160, 10);
     }
 }
--- src/decimal/Dechimal.java
+++ src/decimal/Dechimal.java
@@ -12,11 +12,16 @@ public class Dechimal implements Controller{
     public void calculate(int number, int system) {
         ArrayList result = new ArrayList();
         int temp;
+        char ch = 64; //A - 1
         while(number != 0){
             temp = number % system;
 
             number = (temp > 0) ? (number - temp)/system : number/system;
-            result.add(temp);
+            if(temp > 9){
+                result.add((char) (ch + temp - 9));
+            } else {
+                result.add(temp);
+            }
         }
         Collections.reverse(result);
         System.out.println(result);
